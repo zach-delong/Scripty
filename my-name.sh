d1bb3efe76e9e35b061164c48d2076e8bfce47c2
Zachery DeLong <zacherypd@gmail.com>
2021-07-07 17:59:51 -0400

    my-name updates (formatting)
    - Formatted the script breaking the pipes on lines
    - Documented the script to explain what it should be doing
    - Detailed documentation for why each step is what it is.

--- my-name.sh
+++ my-name.sh
@@ -1,1 +1,11 @@
-cat /etc/passwd | grep $USER | awk -F ':' '{print $5}' | awk -F ',' '{print $1}' | awk -F ' ' '{print $1}'
+#! /bin/zsh
+
+# This script is just designed to pull the currently logged in user's first name
+# from the /etc/passwd file. On popOS, this is in the 5th cell of the file, inside
+# the "historical" data.
+
+cat /etc/passwd | # Read the file
+    grep $USER | # Look for the currently authenticated user in the file
+    awk -F ':' '{print $5}' | # Examine only the 5th cell (should be of the form First Last, ..., with a bunch of stuff I don't care about)
+    awk -F ',' '{print $1}' | # Split the comma delimited list grabbing only the full name
+    awk -F ' ' '{print $1}' # Split the full name just grabbing the first name
