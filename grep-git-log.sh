e2e563ed3d95fba4342b0abc6541adf0e07e8a8b
Zachery DeLong <zacherypd@gmail.com>
2021-09-08 18:31:07 -0400

    The grep cherry pick wasn't working
    This needs to be rethought, but the main functionality of printing commits in
    reverse order is incredibly useful, so I have separated that out into its own
    command

--- grep-git-log.sh
+++ grep-git-log.sh
@@ -1,8 +1,6 @@
-# Parameters! 
-$branchName=$(1-origin/master) # The name of the branch we should look at when
-                               # logging
-$tmpFileName="tmp.txt" # The name of the temporary file to use (delete this when
-                       # you are done!)
+# Parameters!
+branchName=${1-origin/master} # The name of the branch we should look at when
+                              # logging
 
 # We want to run some kind of large git log. We would like it if that log could
 # preserve the order we want to cherry pick our commits in. To do that, we use
@@ -16,12 +14,4 @@ gitLogCommand="git log $branchName --oneline --first-parent $stringSearchPattern
 
 # The exec command will run our code in a string, and we use awk to grab only
 # the commit hash from the output.
-exec $gitLogCommand | awk -F" " '{ print $1 }' > tmp.txt
-
-# tac will iterate over the file (line by line) in reverse. The temp file is
-# currently one commit hash per line with the oldest at the bottom, so we should
-# cherry pick in the reverse order!
-tac tmp.txt | xargs git cherry-pick -m1
-
-# now that we're done, lets clean up
-rm $tmpFileName
+exec $gitLogCommand | tac
