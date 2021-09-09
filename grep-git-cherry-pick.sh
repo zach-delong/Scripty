eeee11b9dcf256bc7c399d9b96da51f94623c63f
Zachery DeLong <zacherypd@gmail.com>
2021-09-08 20:59:59 -0400

    Documentation

--- grep-git-cherry-pick.sh
+++ grep-git-cherry-pick.sh
@@ -3,17 +3,22 @@ commitHashOnlyTmpFile="tmp2.txt"
 alreadyPickedTmpFile="alreadyPicked.txt"
 
 
+# Using the other script in this repo, grab the commits we care about
 grep-git-log.sh < /dev/stdin > $logOutputTmpFile
 
+# Grab only the hashes from that output
 awk -F" " '{ print $1 }' $logOutputTmpFile > $commitHashOnlyTmpFile
 
 
+# If the "already picked" file doesn't exist, create it
 if [ ! -f "$alreadyPickedTmpFile" ]; then
     echo "" > "$alreadyPickedTmpFile"
 else
+    # If it already exists, then filter things that have already been picked out
     grep -v -x -f $alreadyPickedTmpFile f1 > $commitHashOnlyTmpFile
 fi
 
+# While there is input to pick, log that we picked it and attempt to grab!
 while read hash;
 do
     echo "Attempting to pick $hash"
